aa561ac75bd3b33a40cc2de405508a2adb5a6c89
TiagoDuarte25 <tj.duarte@campus.fct.unl.pt>
2022-11-12 15:13:34 +0000

    Auctions with Cache

--- src/scc/srv/AuctionsResource.java
+++ src/scc/srv/AuctionsResource.java
@@ -1,8 +1,12 @@
 package scc.srv;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import jakarta.ws.rs.*;
 
 import jakarta.ws.rs.core.MediaType;
+import redis.clients.jedis.Jedis;
+import scc.cache.RedisCache;
 
 /**
  * Resource for managing auction.
@@ -10,13 +14,21 @@ import jakarta.ws.rs.core.MediaType;
 @Path("/auction")
 public class AuctionsResource {
 
-    private static CosmosDBLayer db_instance = CosmosDBLayer.getInstance();
+    private static CosmosDBLayer db_instance;
+    private static Jedis jedis_instance;
+    private ObjectMapper mapper;
 
     private static String AUCTION_NULL = "Null auction exception";
     private static String AUCTION_NOT_EXIST = "Auction does not exist";
     private static String USER_NOT_EXIST = "User does not exist";
     private static String IMG_NOT_EXIST = "Image does not exist";
 
+
+    public AuctionsResource() {
+        db_instance = CosmosDBLayer.getInstance();
+        jedis_instance = RedisCache.getCachePool().getResource();
+        mapper = new ObjectMapper();
+    }
 
     /**
      * Creates a new auction. The id of the auction is its hash.
@@ -39,8 +51,15 @@ public class AuctionsResource {
             return IMG_NOT_EXIST;
         }
 
-        // Create the user to store in the db
+        // Create the user to store in the db and cache
         AuctionDAO dbAuction = new AuctionDAO(auction);
+
+        try {
+            jedis_instance.set("auction:" + auction.getId(), mapper.writeValueAsString(dbAuction));
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
         db_instance.putAuction(dbAuction);
         return dbAuction.getTitle();
     }
@@ -57,18 +76,33 @@ public class AuctionsResource {
             System.out.println(AUCTION_NULL);
             return AUCTION_NULL;
         }
-        if (!db_instance.getAuctionById(auction.getId()).iterator().hasNext()) {
-            System.out.println(AUCTION_NOT_EXIST);
-            return AUCTION_NOT_EXIST;
-        }
+
         if (!db_instance.getUserById(auction.getOwnerId()).iterator().hasNext()) {
             System.out.println(USER_NOT_EXIST);
             return USER_NOT_EXIST;
         }
         AuctionDAO dbAuction = new AuctionDAO(auction);
-        db_instance.updateAuction(dbAuction);
-        return dbAuction.getId();
+
+        try {
+            String res = jedis_instance.get("auction:"+ auction.getId());
+            if (res != auction.getId() && res != null) {
+                jedis_instance.set("auction:" + auction.getId(), mapper.writeValueAsString(dbAuction));
+                db_instance.updateAuction(dbAuction);
+                return dbAuction.getId();
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+
+        if (!db_instance.getAuctionById(auction.getId()).iterator().hasNext()) {
+            System.out.println(AUCTION_NOT_EXIST);
+            return AUCTION_NOT_EXIST;
+        } else {
+            db_instance.updateAuction(dbAuction);
+            return dbAuction.getId();
+        }
     }
+    
     private boolean verifyImgId(String ImgId) {
         return true;
     }
