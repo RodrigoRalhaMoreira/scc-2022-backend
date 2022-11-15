b53646dac606725eb380083d9788a08a638010ad
dinis58763 <dj.silvestre@campus.fct.unl.pt>
2022-11-15 11:59:15 +0000

    AuctionsResource

--- src/scc/srv/AuctionsResource.java
+++ src/scc/srv/AuctionsResource.java
@@ -1,5 +1,6 @@
 package scc.srv;
 
+import java.lang.reflect.Field;
 import java.util.Iterator;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -28,12 +29,8 @@ public class AuctionsResource {
     private static final String IMG_NOT_EXIST = "Image does not exist";
     private static final String INVALID_STATUS = "Invalid status";
     
-    private static final String NULL_ID = "Null id exception";
-    private static final String NULL_TITLE = "Null title exception";
-    private static final String NULL_IMAGE = "Null imageId exception";
-    private static final String NULL_OWNERID = "Null ownerId exception";
-    private static final String NULL_ENDTIME = "Null endTime exception";
-    private static final String NULL_STATUS = "Null status exception";
+    private static final String NULL = "Null ";
+    private static final String EXCEPTION = " exception";
     private static final String NEGATIVE_MINPRICE = "minPrice can not be negative or zero";
     
     public AuctionsResource() {
@@ -48,11 +45,13 @@ public class AuctionsResource {
 
     /**
      * Creates a new auction. The id of the auction is its hash.
+     * @throws IllegalAccessException 
+     * @throws IllegalArgumentException 
      */
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String create(Auction auction) {
+    public String create(Auction auction) throws IllegalArgumentException, IllegalAccessException {
         
         // Winning bids start by default with the value of null
         
@@ -84,46 +83,42 @@ public class AuctionsResource {
     /**
      * Updates an auction. Throw an appropriate error message if
      * id does not exist.
+     * @throws IllegalAccessException 
+     * @throws IllegalArgumentException 
      */
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String update(Auction auction) {
+    public String update(Auction auction) throws IllegalArgumentException, IllegalAccessException {
         
         String result = checkAuction(auction);
         
         if(result != null)
             return result;
-        
-        else {
 
-            // Status special verification when updating an auction
-            if(!isValidStatus(auction.getStatus()))
-                return INVALID_STATUS;
-    
-            AuctionDAO dbAuction = new AuctionDAO(auction);
-    
-            try {
-                String res = jedis_instance.get("auction:"+ auction.getId());
-                if (res != auction.getId() && res != null) {
-                    jedis_instance.set("auction:" + auction.getId(), mapper.writeValueAsString(dbAuction));
-                    db_instance.updateAuction(dbAuction);
-                    return dbAuction.getId();
-                }
-            } catch (Exception e) {
-                // TODO: handle exception
-            }
-    
-            if (!db_instance.getAuctionById(auction.getId()).iterator().hasNext()) {
-                System.out.println(AUCTION_NOT_EXIST);
-                return AUCTION_NOT_EXIST;
-            } 
-            
-            else {
+        // Status special verification when updating an auction
+        if(!isValidStatus(auction.getStatus()))
+            return INVALID_STATUS;
+        
+        // Checks if auctionId exists
+        if (!db_instance.getAuctionById(auction.getId()).iterator().hasNext()) 
+            return AUCTION_NOT_EXIST;
+
+        AuctionDAO dbAuction = new AuctionDAO(auction);
+
+        try {
+            String res = jedis_instance.get("auction:"+ auction.getId());
+            if (res != auction.getId() && res != null) {
+                jedis_instance.set("auction:" + auction.getId(), mapper.writeValueAsString(dbAuction));
                 db_instance.updateAuction(dbAuction);
                 return dbAuction.getId();
             }
+        } catch (Exception e) {
+            // TODO: handle exception
         }
+        
+        db_instance.updateAuction(dbAuction);
+        return dbAuction.getId();
     }
 
     @GET
@@ -144,13 +139,18 @@ public class AuctionsResource {
                 status.equals(AuctionStatus.DELETED.getStatus())) ? true : false);
     }
     
-    private String checkAuction(Auction auction) {
-        
-        String result = null;
+    private String checkAuction(Auction auction) throws IllegalArgumentException, IllegalAccessException {
         
         if (auction == null) 
-            result = AUCTION_NULL;
+            return AUCTION_NULL;
         
+        for (Field f : auction.getClass().getDeclaredFields()) {
+            f.setAccessible(true);
+            if (f.get(auction) == null && !f.getName().equals("description") && !f.getName().equals("winnigBid"))
+                return NULL + f.getName() + EXCEPTION;
+        }
+        
+        /**
         else if (auction.getId() == null)
             result = NULL_ID;
         
@@ -168,17 +168,18 @@ public class AuctionsResource {
         
         else if (auction.getStatus() == null)
             result = NULL_STATUS;
+        **/
         
-        else if (auction.getMinPrice() <= 0)
-            result = NEGATIVE_MINPRICE;
+        if (auction.getMinPrice() <= 0)
+            return NEGATIVE_MINPRICE;
         
-        else if (!db_instance.getUserById(auction.getOwnerId()).iterator().hasNext())
-            result = USER_NOT_EXIST;
+        if (!db_instance.getUserById(auction.getOwnerId()).iterator().hasNext())
+            return USER_NOT_EXIST;
         
         //verify if imgId exists in the database
-        else if (!media.verifyImgId(auction.getImgId()))
-            result = IMG_NOT_EXIST;
+        if (!media.verifyImgId(auction.getImgId()))
+            return IMG_NOT_EXIST;
         
-        return result;
+        return null;
     }
 }
