2e83296b06de9c11787b83c6a950b3aab6a804e7
TiagoDuarte25 <tj.duarte@campus.fct.unl.pt>
2022-11-18 19:21:25 +0000

    Login Not Otimizated

--- src/scc/cache/RedisCache.java
+++ src/scc/cache/RedisCache.java
@@ -2,7 +2,6 @@ package scc.cache;
 
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
-import scc.srv.Session;
 
 public class RedisCache {
 	private static final String RedisHostname = "rediswesteuropegroupdrt.redis.cache.windows.net";
@@ -25,9 +24,5 @@ public class RedisCache {
 		instance = new JedisPool(poolConfig, RedisHostname, 6380, 1000, RedisKey, true);
 		return instance;
 
-	}
-
-	public Session getSession(String session) {
-		return null;
 	}
 }
